ae5c149dddfaf605a522dd1698a69f3204d98a40
Danylo Goncharov <danylo.goncharov@xing.com>
2023-04-13 21:48:36 +0200

    bottom up

--- src/google.foo.bar/level3/find_the_access_codes.py
+++ src/google.foo.bar/level3/find_the_access_codes.py
@@ -1,29 +1,13 @@
-NOT_SET = -1
-
-
 def solution(items):
-    cache = [[NOT_SET for _ in range(len(items))] for _ in range(3)]
-    return count_lucky_numbers(items, 0, 1, 2, cache)
+    cache = [[0 for _ in range(len(items))] for _ in range(2)]
 
+    for i in range(1, len(items)):
+        for j in range(i - 1, -1, -1):
+            if items[i] % items[j] == 0:
+                cache[0][i] += 1
 
-def count_lucky_numbers(
-        items,
-        index,
-        previous_element,
-        sequence_counter,
-        cache
-):
-    if index == len(items):
-        return 0
-    if cache[sequence_counter][index] != NOT_SET:
-        return cache[sequence_counter][index]
-    if items[index] % previous_element != 0:
-        return 0
-
-    if sequence_counter == 0 and items[index] % previous_element == 0:
-        return 1
-    counter = 0
-    for i in range(index + 1, len(items)):
-        counter += count_lucky_numbers(items, i, items[index], sequence_counter - 1, cache)
-    cache[sequence_counter][index] = counter
-    return cache[sequence_counter][index]
+    for i in range(2, len(items)):
+        for j in range(i - 1, -1, -1):
+            if items[i] % items[j] == 0:
+                cache[1][i] += cache[0][j]
+    return sum(cache[1])
--- src/google.foo.bar/main.py
+++ src/google.foo.bar/main.py
@@ -3,7 +3,7 @@ from level3.find_the_access_codes import solution
 
 def print_hi(name):
     print(solution(range(1, 7, 1)))
-    print(solution(range(1, 10000, 1)))
+    # print(solution(range(1, 10000, 1)))
 
 
 # Press the green button in the gutter to run the script.
