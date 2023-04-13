f2e3d19b62c050d5f4d492bfc9db8114fa20c8b5
Danylo Goncharov <danylo.goncharov@xing.com>
2023-04-13 21:34:21 +0200

    top to bottom

--- src/google.foo.bar/level3/find_the_access_codes.py
+++ src/google.foo.bar/level3/find_the_access_codes.py
@@ -1,10 +1,22 @@
+NOT_SET = -1
+
+
 def solution(items):
-    return count_lucky_numbers(items, 0, 1, 2)
+    cache = [[NOT_SET for _ in range(len(items))] for _ in range(3)]
+    return count_lucky_numbers(items, 0, 1, 2, cache)
 
 
-def count_lucky_numbers(items, index, previous_element, sequence_counter):
+def count_lucky_numbers(
+        items,
+        index,
+        previous_element,
+        sequence_counter,
+        cache
+):
     if index == len(items):
         return 0
+    if cache[sequence_counter][index] != NOT_SET:
+        return cache[sequence_counter][index]
     if items[index] % previous_element != 0:
         return 0
 
@@ -12,5 +24,6 @@ def count_lucky_numbers(items, index, previous_element, sequence_counter):
         return 1
     counter = 0
     for i in range(index + 1, len(items)):
-        counter += count_lucky_numbers(items, i, items[index], sequence_counter - 1)
-    return counter
+        counter += count_lucky_numbers(items, i, items[index], sequence_counter - 1, cache)
+    cache[sequence_counter][index] = counter
+    return cache[sequence_counter][index]
--- src/google.foo.bar/main.py
+++ src/google.foo.bar/main.py
@@ -2,8 +2,8 @@ from level3.find_the_access_codes import solution
 
 
 def print_hi(name):
-    print(solution([1, 2, 3, 4, 5, 6]))
-
+    print(solution(range(1, 7, 1)))
+    print(solution(range(1, 10000, 1)))
 
 
 # Press the green button in the gutter to run the script.
