c22a07035cebac34ecb49a1448e45e6ae24196a2
Danylo Goncharov <danylo.goncharov@xing.com>
2023-04-16 15:31:54 +0200

    replace subtraction with division

--- src/google.foo.bar/level3/bomb_baby.py
+++ src/google.foo.bar/level3/bomb_baby.py
@@ -1,20 +1,26 @@
-IMPOSSIBLE_RESULT = "impossible"
-
-
 def solution(mach_needed_str, facula_needed_str):
     mach_needed = int(mach_needed_str)
     facula_needed = int(facula_needed_str)
 
-    if mach_needed <= 0 or facula_needed <= 0:
-        return IMPOSSIBLE_RESULT
-
     counter = 0
+
     while mach_needed > 0 and facula_needed > 0:
         if mach_needed == 1 and facula_needed == 1:
             return str(counter)
         if mach_needed > facula_needed:
-            mach_needed -= facula_needed
+            counter_increment, new_max = calculate_step(mach_needed, facula_needed)
+            mach_needed = new_max
         else:
-            facula_needed -= mach_needed
-        counter += 1
-    return IMPOSSIBLE_RESULT
+            counter_increment, new_max = calculate_step(facula_needed, mach_needed)
+            facula_needed = new_max
+        counter += counter_increment
+
+    return "impossible"
+
+
+def calculate_step(max_number, min_number):
+    diff = max_number - min_number
+    counter_increment = diff / min_number
+    counter_increment = 1 if counter_increment == 0 else counter_increment
+    new_max = max_number - counter_increment * min_number
+    return counter_increment, new_max
--- src/google.foo.bar/main.py
+++ src/google.foo.bar/main.py
@@ -5,8 +5,6 @@ def print_hi(name):
     print(solution('2', '1'))
     print(solution('4', '7'))
     print(solution('2', '4'))
-    # print(solution(range(1, 10000, 1)))
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
