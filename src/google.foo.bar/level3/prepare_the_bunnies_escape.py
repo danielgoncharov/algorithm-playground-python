17c337b1d07d979c90131863f4adacb38991bcc7
danylo.goncharov <danylo.goncharov@xing.com>
2023-04-18 09:32:44 +0200

    with penetration limit

--- src/google.foo.bar/level3/prepare_the_bunnies_escape.py
+++ src/google.foo.bar/level3/prepare_the_bunnies_escape.py
@@ -2,52 +2,86 @@ from collections import deque
 
 
 def solution(maze):
-    min_path = bfs(maze)
-    rows, cols = get_dim(maze)
-    for i in range(rows):
-        for j in range(cols):
-            if maze[i][j] == 1:
-                maze[i][j] = 0
-                min_path = min(min_path, bfs(maze))
-                maze[i][j] = 1
-    return min_path
+    source = Node(0, 0, 1, maze)
+    queue = deque([source])
+    distance_map = {source: 1}
 
-
-def bfs(maze):
-    graph = to_graph(maze)
-    visited = set()
-    start = (0, 0)
-    rows, cols = get_dim(maze)
-    end = (rows - 1, cols - 1)
-    queue = deque([(start, [])])
     while queue:
-        node, path = queue.popleft()
-        if node == end:
-            return len(path)+1
-        visited.add(node)
-        for neighbor in graph[node]:
-            if neighbor not in visited:
-                queue.append((neighbor, path + [neighbor]))
+        current_node = queue.popleft()
+
+        if current_node.is_exit_pod():
+            return distance_map[current_node]
+
+        for child_node in current_node.neighbors():
+            if child_node not in distance_map.keys():
+                distance_map[child_node] = distance_map[current_node] + 1
+                queue.append(child_node)
+
     return None
 
 
-def get_dim(maze):
-    return len(maze), len(maze[0])
+class Node:
 
+    def __init__(self, x, y, pen_limit, maze):
+        self.x = x
+        self.y = y
+        self.pen_limit = pen_limit
+        self.maze = maze
 
-def to_graph(maze):
-    rows, cols = get_dim(maze)
-    graph = {}
-    for i in range(rows):
-        for j in range(cols):
-            neighbors = []
-            if i > 0 and not (maze[i][j] == 1 and maze[i - 1][j] == 1):
-                neighbors.append((i - 1, j))
-            if i < rows - 1 and not (maze[i][j] == 1 and maze[i + 1][j] == 1):
-                neighbors.append((i + 1, j))
-            if j > 0 and not (maze[i][j] == 1 and maze[i][j - 1] == 1):
-                neighbors.append((i, j - 1))
-            if j < cols - 1 and not (maze[i][j] == 1 and maze[i][j + 1] == 1):
-                neighbors.append((i, j + 1))
-            graph[(i, j)] = neighbors
-    return graph
+    def __hash__(self):
+        return self.x ^ self.y
+
+    def rows(self):
+        return len(self.maze)
+
+    def columns(self):
+        return len(self.maze[0])
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def is_exit_pod(self):
+        return self.x == self.columns() - 1 and self.y == self.rows() - 1
+
+    def neighbors(self):
+        neighbors = []
+        x = self.x
+        y = self.y
+        pen_limit = self.pen_limit
+        maze = self.maze
+        rows = self.rows()
+        columns = self.columns()
+
+        if x > 0:
+            wall = maze[y][x - 1] == 1
+            if wall:
+                if pen_limit > 0:
+                    neighbors.append(Node(x - 1, y, pen_limit - 1, maze))
+            else:
+                neighbors.append(Node(x - 1, y, pen_limit, maze))
+
+        if x < columns - 1:
+            wall = maze[y][x + 1] == 1
+            if wall:
+                if pen_limit > 0:
+                    neighbors.append(Node(x + 1, y, pen_limit - 1, maze))
+            else:
+                neighbors.append(Node(x + 1, y, pen_limit, maze))
+
+        if y > 0:
+            wall = maze[y - 1][x] == 1
+            if wall:
+                if pen_limit > 0:
+                    neighbors.append(Node(x, y - 1, pen_limit - 1, maze))
+            else:
+                neighbors.append(Node(x, y - 1, pen_limit, maze))
+
+        if y < rows - 1:
+            wall = maze[y + 1][x]
+            if wall:
+                if pen_limit > 0:
+                    neighbors.append(Node(x, y + 1, pen_limit - 1, maze))
+            else:
+                neighbors.append(Node(x, y + 1, pen_limit, maze))
+
+        return neighbors
