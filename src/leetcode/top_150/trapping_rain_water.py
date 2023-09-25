6a8f7a16fac65dbbcb024e965e4749a4d65bced1
danylo.goncharov <danylo.goncharov@new-work.se>
2023-09-25 20:45:48 +0200

    trapping water

--- src/leetcode/top_150/trapping_rain_water.py
+++ src/leetcode/top_150/trapping_rain_water.py
@@ -2,19 +2,22 @@ from typing import List
 
 
 class TrappingRainWater:
-    def trap(self, heights: List[int]) -> int:
-        left_elevation = heights[0]
-        result = 0
-        decrement = 0
-        for index in range(1, len(heights)):
-            right_elevation = index
-            if heights[right_elevation] > heights[left_elevation] and right_elevation - left_elevation == 1:
-                left_elevation = right_elevation
-            elif heights[right_elevation] > heights[left_elevation]:
-                result += (right_elevation - left_elevation - 1) * heights[left_elevation] - decrement
-                left_elevation = right_elevation
-                decrement = 0
-            else:
-                decrement += heights[right_elevation]
 
+    def trap(self, heights: List[int]) -> int:
+        left_support = [0] * len(heights)
+        right_support = [0] * len(heights)
+
+        max_wall = -1
+        for index in range(0, len(heights)):
+            max_wall = max(max_wall, heights[index])
+            left_support[index] = max_wall
+
+        max_wall = -1
+        for index in range(len(heights) - 1, -1, -1):
+            max_wall = max(max_wall, heights[index])
+            right_support[index] = max_wall
+
+        result = 0
+        for index in range(0, len(heights)):
+            result += min(left_support[index], right_support[index]) - heights[index]
         return result
