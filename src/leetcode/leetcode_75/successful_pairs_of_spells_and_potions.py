499ab561e2b3a2f2a1a90b08ba70916ef52ab32c
danylo.goncharov <danylo.goncharov@new-work.se>
2024-02-12 21:43:00 +0100

    SuccessfulPairsOfSpellsAndPotions

--- src/leetcode/leetcode_75/successful_pairs_of_spells_and_potions.py
+++ src/leetcode/leetcode_75/successful_pairs_of_spells_and_potions.py
@@ -1,7 +1,8 @@
 from bisect import bisect_left
+from math import ceil
 
 
 class SuccessfulPairsOfSpellsAndPotions:
     def successfulPairs(self, spells, potions, success):
         potions.sort()
-        return [len(potions) - bisect_left(potions, (success + spell - 1) // spell) for spell in spells]
+        return [len(potions) - bisect_left(potions, ceil(success / spell)) for spell in spells]
