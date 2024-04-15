60d4dac60da7c6f696e9c909759d9375e08ed576
danylo.goncharov <danylo.goncharov@new-work.se>
2024-04-15 20:29:23 +0200

    two array diff

---
+++ src/leetcode/leetcode_75/FindDifferenceOfTwoArrays.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class FindDifferenceOfTwoArrays:
+    def findDifference(
+            self,
+            nums1: List[int],
+            nums2: List[int]
+    ) -> List[List[int]]:
+        nums1_set = set(nums1)
+        nums2_set = set(nums2)
+        result = [
+            list(
+                nums1_set.difference(nums2_set)
+            ),
+            list(
+                nums2_set.difference(nums1_set)
+            )
+        ]
+        return result
