0916cc63d039ec41a6cd054e3be4ba7ff83efd47
danylo.goncharov <danylo.goncharov@new-work.se>
2024-07-25 20:48:03 +0200

    tab

--- src/leetcode/leetcode_75/LongestCommonSubsequence.py
+++ src/leetcode/leetcode_75/LongestCommonSubsequence.py
@@ -9,10 +9,11 @@ class LongestCommonSubsequence:
 
         for i in range(1, len(text2) + 1):
             for j in range(1, len(text1) + 1):
-                memory_array[i][j] = max(
-                    memory_array[i - 1][j],
-                    memory_array[i][j - 1]
-                )
                 if text2[i - 1] == text1[j - 1]:
-                    memory_array[i][j] = memory_array[i][j] + 1
+                    memory_array[i][j] = memory_array[i - 1][j - 1] + 1
+                else:
+                    memory_array[i][j] = max(
+                        memory_array[i - 1][j],
+                        memory_array[i][j - 1]
+                    )
         return memory_array[len(text2)][len(text1)]
