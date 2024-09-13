87c486383dc2c8813bdd684091931b454282e640
danylo.goncharov <danylo.goncharov@new-work.se>
2024-09-13 12:16:18 +0200

    MaxConsecutiveOnes

---
+++ src/leetcode/leetcode_75/max_consecutive_ones.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class MaxConsecutiveOnes:
+
+    def longestOnes(
+            self,
+            nums: List[int],
+            k: int
+    ) -> int:
+        left = right = 0
+
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                k -= 1
+
+            if k < 0:
+                if nums[left] == 0:
+                    k += 1
+                left += 1
+
+        return right - left + 1
+
