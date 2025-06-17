27267a3dd304b28109583f65b4d80d0e4197aa89
danylo.goncharov <danylo.goncharov@new-work.se>
2025-06-17 20:44:22 +0200

    BinaryTreeRightSideVie

---
+++ src/leetcode/leetcode_75/binary_tree_right_side_view.py
@@ -0,0 +1,31 @@
+from typing import Optional, List
+
+from src.leetcode.linkedlists.tree_node import TreeNode
+from collections import deque
+
+
+class BinaryTreeRightSideVie:
+
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+        result_with_index = [(root.val, 0)]
+        processing_dequeue = deque([(root, 0)])
+        while processing_dequeue:
+            (current_node, current_level) = processing_dequeue.popleft()
+            if current_node is None:
+                continue
+            last_element_index = len(result_with_index) - 1
+            (last_value, last_level) = result_with_index[last_element_index]
+            if current_level == last_level:
+                result_with_index.insert(last_element_index, (current_node.val, current_level))
+            else:
+                result_with_index.append((current_node.val, current_level))
+            processing_dequeue.append(
+                (current_node.left, current_level + 1)
+            )
+
+            processing_dequeue.append(
+                (current_node.right, current_level + 1)
+            )
+        return [item[0] for item in result_with_index]
--- src/leetcode/main.py
+++ src/leetcode/main.py
@@ -1,17 +1,8 @@
-from src.leetcode.leetcode_75.combination_sum import CombinationSum
-from src.leetcode.leetcode_75.LongestCommonSubsequence import LongestCommonSubsequence
-from src.leetcode.leetcode_75.counting_bits import CountingBits
-from src.leetcode.leetcode_75.delete_the_middle_node_of_a__linked_list import DeleteTheMiddleNodeOfALinkedList
-from src.leetcode.leetcode_75.find_pivot_index import FindPivotIndex
-from src.leetcode.leetcode_75.house_robber import HouseRobber
-from src.leetcode.leetcode_75.koko_eating_bananas import KokoEatingBananas
-from src.leetcode.leetcode_75.removing_stars_from_a_string import RemovingStarsFromAString
-from src.leetcode.leetcode_75.tribonacci_number import TribonacciNumber
-from src.leetcode.leetcode_75.unique_number_of_occurrences import UniqueNumberOfOccurrences
+from src.leetcode.leetcode_75.binary_tree_right_side_view import BinaryTreeRightSideVie
 
 
 def run():
-    result = UniqueNumberOfOccurrences().uniqueOccurrences([1,2])
+    result = BinaryTreeRightSideVie().rightSideView([1,2])
     print(result)
 
 
