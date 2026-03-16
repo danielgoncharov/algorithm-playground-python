9350eafc6f5601682e785d5aa1032c0ff789bb4d
danylo.goncharov <danylo.goncharov@new-work.se>
2026-03-16 21:48:22 +0100

    EditDistance

--- src/leetcode/leetcode_75/edit_distance.py
+++ src/leetcode/leetcode_75/edit_distance.py
@@ -1,24 +1,23 @@
 class EditDistance:
 
     def minDistance(self, word1: str, word2: str) -> int:
-        return self.find_min_distance(0, 0, word1, word2)
+        return self.find_min_distance(word1, word2, 0, 0, [])
 
-    def find_min_distance(
-            self,
-            word1_index: int,
-            word2_index: int,
-            word1: str,
-            word2: str
-    ) -> int:
-        if word1_index == len(word1) and word2_index == len(word2):
+    def find_min_distance(self, word1, word2, i, j, memo):
+        if i == len(word1) and j == len(word2):
             return 0
-        if word1_index == len(word1):
-            return len(word2) - word2_index
-        if word2_index == len(word2):
-            return len(word1) - word1_index
+        if i == len(word1):
+            return len(word2) - j
+        if j == len(word2):
+            return len(word1) - i
 
-        return min(
-            self.find_min_distance(word1_index + 1, word2_index + 1, word1, word2),
-            self.find_min_distance(word1_index, word2_index + 1, word1, word2),
-            self.find_min_distance(word1_index + 1, word2_index, word1, word2),
-        ) + 1
+        if (i, j) not in memo:
+            if word1[i] == word2[j]:
+                ans = self.find_min_distance(word1, word2, i + 1, j + 1, memo)
+            else:
+                insert = 1 + self.find_min_distance(word1, word2, i, j + 1, memo)
+                delete = 1 + self.find_min_distance(word1, word2, i + 1, j, memo)
+                replace = 1 + self.find_min_distance(word1, word2, i + 1, j + 1, memo)
+                ans = min(insert, delete, replace)
+            memo[(i, j)] = ans
+        return memo[(i, j)]
